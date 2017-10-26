95321d773417346a4627a05574e304e4fae36f91
Gilles QUERRET <g.querret@riverside-software.fr>
2017-10-26 17:28:25 +0200

    Update config.groovy

--- src/main/resources/jenkinsci/plugin/openedge/OpenEdgeBuildWrapper/config.groovy
+++ src/main/resources/jenkinsci/plugin/openedge/OpenEdgeBuildWrapper/config.groovy
@@ -1,7 +1,7 @@
 package jenkinsci.plugin.openedge;
 def f = namespace(lib.FormTagLib)
 def installationsDefined = descriptor.installations.length != 0
-def title = installationsDefined ? _("OpenEdge version") : _("No setup will be done, as no Go installations have been defined in the Jenkins system config")
+def title = installationsDefined ? _("OpenEdge version") : _("No setup will be done, as no OpenEdge installations have been defined in the Jenkins system config")
 f.entry(title: title) {
  if (installationsDefined) {
   select(class:"setting-input", name:".openEdgeInstall") {
